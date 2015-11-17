a96337a69a9867c9ebfcd89899384af6a30e8df3
Nima Kian <nimakian@comcast.net>
2015-11-16 23:21:58 -0700

    Create multiplechoicequestion.java

---
+++ multiplechoicequestion.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+
+
+public class ChoiceQuestion extends Question
+{
+  
+  
+   public ChoiceQuestion()
+   {
+      choose question from the array
+   }
+
+   public void addChoice(String choice, boolean correct)
+   {
+     add the answers or options to the question 
+   }
+   public void display()
+   {
+     display the choices    
+      }
+   }
+}
+
