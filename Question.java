32b5ab9d28537584f6d4fd5a16ac6be5c780775c
Nima Kian <nimakian@comcast.net>
2015-11-16 23:16:39 -0700

    Create Question.java

---
+++ Question.java
@@ -0,0 +1,29 @@
+public class Questions
+{
+  private String text;
+  private String answer;
+  
+  public Questions()
+  {}
+    
+  public void text(String question)
+  {
+    Generate Question
+  }
+    
+  public void answer(String answer)  
+  {
+    Generate answer to question
+  }
+  
+  public boolean checkAnswer(String response)
+  {
+    the response equals = answer? correct
+      other wise incorrect
+  }
+  
+  public void display()
+  { 
+   print question
+  }
+  }
